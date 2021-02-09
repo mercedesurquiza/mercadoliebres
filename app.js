4599637cd722b09e128963f6524396b637f44249
mercedesurquiza <urquizamer@gmail.com>
2021-02-09 20:24:20 -0300

    proyecto terminado

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express")
 const path = require("path")
 const app = express()
 
-app.listen(3016);
+app.listen(process.env.PORT ||3018);
 
 const staticFolder = path.resolve(__dirname, "./public")
 app.use(express.static(staticFolder));
